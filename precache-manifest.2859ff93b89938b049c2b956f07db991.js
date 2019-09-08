60d6fdbbbd37529e85621e07ecd9769bb17e6bc3
NofarPeled <nofarpeled4@gmail.com>
2019-09-08 12:50:18 +0300

    Updates

---
+++ precache-manifest.2859ff93b89938b049c2b956f07db991.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c58e10e8d85fa27d11d5b55b1075cbe",
+    "url": "/Nofar-peled-09-02-2019/index.html"
+  },
+  {
+    "revision": "69f432d919ccba1e437a",
+    "url": "/Nofar-peled-09-02-2019/static/css/main.377cbac6.chunk.css"
+  },
+  {
+    "revision": "204d69f2030d63a08ac0",
+    "url": "/Nofar-peled-09-02-2019/static/js/2.c32d8397.chunk.js"
+  },
+  {
+    "revision": "69f432d919ccba1e437a",
+    "url": "/Nofar-peled-09-02-2019/static/js/main.999b4d27.chunk.js"
+  },
+  {
+    "revision": "4e497cbb1f0b14923cf8",
+    "url": "/Nofar-peled-09-02-2019/static/js/runtime~main.ecdd918c.js"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/Nofar-peled-09-02-2019/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "9a77fbaa85fa42b73e3b96399daf49c5",
+    "url": "/Nofar-peled-09-02-2019/static/media/Lato-Thin.9a77fbaa.ttf"
+  },
+  {
+    "revision": "c57e537a1c25ff6c5beb211ef1c86634",
+    "url": "/Nofar-peled-09-02-2019/static/media/PoiretOne-Regular.c57e537a.ttf"
+  },
+  {
+    "revision": "d654ce194e0660d047d6913124f8d562",
+    "url": "/Nofar-peled-09-02-2019/static/media/favorite.d654ce19.svg"
+  },
+  {
+    "revision": "9a7115fb09b43dac9f4b92b4430a12d3",
+    "url": "/Nofar-peled-09-02-2019/static/media/logo.9a7115fb.png"
+  },
+  {
+    "revision": "9d53d97bb11e5a9b3b6014b0f1d6e46e",
+    "url": "/Nofar-peled-09-02-2019/static/media/not-favorite.9d53d97b.svg"
+  },
+  {
+    "revision": "e80ecd75d2d5001172e1c9b422a7929d",
+    "url": "/Nofar-peled-09-02-2019/static/media/weather-page-wallpaper.e80ecd75.jpg"
+  }
+]);
